2dfc31d4c8c50852fb404ba17bbcfdb8e0e513d8
vishnuprksh <vishnucheppanam@gmail.com>
2023-05-31 16:02:05 +0530

    first

---
+++ main.js
@@ -0,0 +1,49 @@
+
+// slide images
+
+const sliderContent = document.querySelector('.slider-content');
+const slides = document.querySelectorAll('.slide');
+const slideWidth = slides[0].clientWidth;
+let counter = 0;
+const sliderDots = document.querySelectorAll('.slider-dot');
+
+
+function nextSlide() {
+    if (counter === slides.length - 1) {
+        counter = 0;
+    } else {
+        counter++;
+        sliderContent.style.transition = 'transform 0.5s ease-in-out';
+    }
+    sliderContent.style.transform = `translateX(${-slideWidth * counter}px)`;
+
+    updateSliderDots();
+}
+
+function previousSlide() {
+    if (counter === 0) {
+        counter = slides.length - 1;
+        sliderContent.style.transition = 'transform 0.5s ease-in-out';
+        sliderContent.style.transform = `translateX(${-slideWidth * counter}px)`;
+    } else {
+        counter--;
+        sliderContent.style.transition = 'transform 0.5s ease-in-out';
+        sliderContent.style.transform = `translateX(${-slideWidth * counter}px)`;
+    }
+    updateSliderDots();
+}
+
+
+//adding silver dots
+
+function updateSliderDots() {
+    sliderDots.forEach((dot, index) => {
+        dot.classList.toggle('active', index === counter);
+    });
+}
+
+//button for selecting language
+function selectLanguage(language) {
+    var selectedLanguageBtn = document.getElementById('selectedLanguageBtn');
+    selectedLanguageBtn.textContent = language;
+}
